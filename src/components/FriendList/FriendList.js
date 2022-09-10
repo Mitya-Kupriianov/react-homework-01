6d6fc705199e91aecd25bc65294fa640d7196e35
Mitya-Kupriianov <106350869+Mitya-Kupriianov@users.noreply.github.com>
2022-09-10 11:40:26 +0300

    Update FriendList.js

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,23 +1,21 @@
 import s from './FriendList.module.css';
 import PropTypes from 'prop-types';
 
-function FriendListItem({ isOnline, avatar, name }) {
+function FriendListItem({ id, isOnline, avatar, name }) {
   return (
-    <>
+    <li key={id} className={s.item}>
       <span className={isOnline ? s.online : s.offline}></span>
       <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={s.name}>{name}</p>
-    </>
+    </li>
   );
 }
 
 export default function FriendList({ friends }) {
   return (
     <ul className={s.friend_list}>
-      {friends.map(({ id, isOnline, avatar, name }) => (
-        <li key={id} className={s.item}>
-          <FriendListItem isOnline={isOnline} avatar={avatar} name={name} />
-        </li>
+      {friends.map(({ isOnline, avatar, name }) => (
+        <FriendListItem isOnline={isOnline} avatar={avatar} name={name} />
       ))}
     </ul>
   );
